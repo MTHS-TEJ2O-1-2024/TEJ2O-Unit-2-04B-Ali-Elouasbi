2aa66eb5f5ebe376af78387544e10857c9faa8b1
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-09-19 11:22:29 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Sep 2024
+ * This program simulates the temperature
 */
 
-basic.showString('Hello, World!')
+// our variable for a random number
+let randomNumber: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    
