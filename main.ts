74fefdc13bd2a1c4a56c0c45ee12073a3da6fdf3
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-09-20 10:53:38 -0400

    added 2-04

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Ali
  * Created on: Sep 2024
- * This program simulates the temperature
+ * This program says the temperature
 */
 
 // our variable for a random number
-let randomNumber: number
+let temperature = 0
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    
+    temperature = input.temperature()
+    basic.showString('Temp=')
+    basic.showNumber(input.temperature())
+})
